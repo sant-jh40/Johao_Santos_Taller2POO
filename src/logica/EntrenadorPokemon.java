cc0b2560678902397871b8274e4736cbc826c631
johao <johao@PcJohaoSantos>
2026-05-08 11:35:20 -0400

    Creacion clase "Entrenador Pokemon"

---
+++ src/logica/EntrenadorPokemon.java
@@ -0,0 +1,23 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class EntrenadorPokemon {
+	protected String nombre;
+	protected ArrayList<Pokémones> equipoPokemonDelEntrenador;
+	
+	public EntrenadorPokemon(String nombre) {
+		this.nombre = nombre;
+		this.equipoPokemonDelEntrenador = new ArrayList<>();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public ArrayList<Pokémones> getEquipoPokemonDelEntrenador() {
+		return equipoPokemonDelEntrenador;
+	}
+	public void agregarPokemonesCapturados(Pokémones capturado) {
+		equipoPokemonDelEntrenador.add(capturado);
+	}
+	public abstract String presentarse();
+}
